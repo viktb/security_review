b569fa9d6ba5d2b4542fde2dee4faa94b1890127
banviktor <viktor.ban@gmail.com>
2015-05-01 19:10:05 +0200

    logEnabled() implemented, functions documented

--- src/SecurityReview.php
+++ src/SecurityReview.php
@@ -5,10 +5,23 @@ namespace Drupal\security_review;
 use Drupal\Core\Session\AccountInterface;
 
 class SecurityReview {
+  /**
+   * @return bool Returns whether logging is enabled.
+   */
+  public static function logEnabled(){
+    return \Drupal::config('security_review.settings')->get('log') === true;
+  }
+
+  /**
+   * @return array Stored untrusted roles.
+   */
   public static function untrustedRoles(){
     return \Drupal::config('security_review.settings')->get('untrusted_roles');
   }
 
+  /**
+   * @return array Default untrusted roles.
+   */
   public static function defaultUntrustedRoles(){
     $roles = array(AccountInterface::ANONYMOUS_ROLE => t('anonymous user'));
 
