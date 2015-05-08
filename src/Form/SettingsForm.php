ba6436a31a6166105ca8ead3ac50d498888e3bd3
banviktor <viktor.ban@gmail.com>
2015-05-08 20:42:17 +0200

    Check-specific settings are the checks' responsibilities.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -82,17 +82,7 @@ class SettingsForm extends ConfigFormBase {
       '#tree' => TRUE,
     );
 
-    // TODO: this will be implemented inside the actual check.
-    $form['advanced']['check_settings']['base_url.method'] = array(
-      '#type' => 'radios',
-      '#title' => t('Base URL check method'),
-      '#description' => t('Detecting the $base_url in settings.php can be done via PHP tokenization (recommend) or including the file. Note that if you have custom functionality in your settings.php it will be executed if the file is included. Including the file can result in a more accurate $base_url check if you wrap the setting in conditional statements.'),
-      '#options' => array(
-        'token' => t('Tokenize settings.php (recommended)'),
-        'include' => t('Include settings.php'),
-      ),
-      '#default_value' => $check_settings->get('base_url.method'),
-    );
+    // TODO: Iterate through checklist and include check-specific setting pages.
 
     // Return the finished form.
     return parent::buildForm($form, $form_state);
@@ -116,10 +106,7 @@ class SettingsForm extends ConfigFormBase {
     // Save the new logging setting.
     $settings->set('log', $form_state->getValue('logging') == 1);
 
-    // Save the check-specific settings.
-    foreach ($form_state->getValue('check_settings') as $variableName => $value) {
-      $check_settings->set($variableName, $value);
-    }
+    // TODO: Save the check-specific settings.
 
     // Commit the settings.
     $settings->save();
