c3e91d0f79b018ceac1fd174f844da7d55319ef6
banviktor <viktor.ban@gmail.com>
2015-06-04 14:43:01 +0200

    Implemented CheckResult.

---
+++ src/CheckResult.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\security_review\CheckResult.
+ */
+
+namespace Drupal\security_review;
+
+/**
+ * Used to define the result of a Check.
+ */
+class CheckResult {
+  const SUCCESS = 0;
+  const FAIL = 1;
+  const WARN = 2;
+  const INFO = 3;
+
+  /**
+   * @var \Drupal\security_review\Check $check
+   */
+  private $check;
+
+  /**
+   * @var int $result
+   */
+  private $result;
+
+  /**
+   * @var array $findings
+   */
+  private $findings;
+
+  /**
+   * @var int $time
+   */
+  private $time;
+
+  /**
+   * @param \Drupal\security_review\Check $check
+   *   The Check that created this result.
+   * @param $result
+   *   The result integer (see the constants defined above).
+   * @param array $findings
+   *   The findings.
+   * @param null $time
+   *   The time the test was run.
+   */
+  public function __construct(Check $check, $result, array $findings, $time = null){
+    // Set the parent check.
+    $this->check = $check;
+
+    // Set the result value.
+    $result = intval($result);
+    if($result < self::SUCCESS | $result > self::INFO){
+      $result = self::INFO;
+    }
+    $this->result = $result;
+
+    // Set the findings.
+    $this->findings = $findings;
+
+    // Set the timestamp.
+    if($time == null){
+      $this->time = time();
+    }else{
+      $this->time = intval($time);
+    }
+  }
+
+  /**
+   * Combines two CheckResults and returns a new one with the old one's fields
+   * except for the findings which are copied from the fresh result.
+   *
+   * @param \Drupal\security_review\CheckResult $old
+   *   The old result to clone.
+   * @param \Drupal\security_review\CheckResult $fresh
+   *   The new result to copy the findings from.
+   *
+   * @return \Drupal\security_review\CheckResult
+   *   The combined result.
+   */
+  public static function combine(CheckResult $old, CheckResult $fresh){
+    return new CheckResult($old->check, $old->result, $fresh->findings, $old->time);
+  }
+
+  /**
+   * @return \Drupal\security_review\Check
+   *   The Check that created this result.
+   */
+  public function check(){
+    return $this->check;
+  }
+
+  /**
+   * @return int
+   *   The result integer.
+   */
+  public function result(){
+    return $this->result;
+  }
+
+  /**
+   * @return array
+   *   The findings. Contents of this depends on the actual check.
+   */
+  public function findings(){
+    return $this->findings;
+  }
+
+  /**
+   * @return int
+   *   The time the result was generated.
+   */
+  public function time(){
+    return $this->time;
+  }
+}
