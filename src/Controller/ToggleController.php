7f15bb1e6d00504556d092e6fe64be3128c9663b
Viktor Bán <viktor.ban@gmail.com>
2015-06-10 10:05:37 +0200

    ToggleController cleaned up.

--- src/Controller/ToggleController.php
+++ src/Controller/ToggleController.php
@@ -1,24 +1,32 @@
 <?php
 
+/**
+ * @file
+ * Contains \Drupal\security_review\Controller\ToggleController.
+ */
+
 namespace Drupal\security_review\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\security_review\Checklist;
 
+/**
+ * Responsible for handling the toggle links on the Run & Review page.
+ */
 class ToggleController extends ControllerBase {
-  public function index($js, $check_id){
+  public function index($js, $check_id) {
     // Determine access type.
     $ajax = $js != 'nojs';
 
     // Validate token.
     $token = \Drupal::request()->query->get('token');
-    if(\Drupal::csrfToken()->validate($token, $check_id)){
+    if (\Drupal::csrfToken()->validate($token, $check_id)) {
       $check = Checklist::getCheckByIdentifier($check_id);
-      if($check != null){
-        if($check->isSkipped()){
+      if ($check != NULL) {
+        if ($check->isSkipped()) {
           $check->enable();
           drupal_set_message(t($check->getTitle() . ' check no longer skipped.'));
-        }else{
+        } else {
           $check->skip();
           drupal_set_message(t($check->getTitle() . ' check skipped.'));
         }
@@ -26,9 +34,9 @@ class ToggleController extends ControllerBase {
     }
 
     // Output.
-    if($ajax) {
+    if ($ajax) {
       // TODO: Print json output.
-    }else{
+    } else {
       // Redirect back to Run & Review.
       return $this->redirect('security_review');
     }
