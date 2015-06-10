4a6446afb05e128d3b0f4245d590b0ba83e21960
Viktor Bán <viktor.ban@gmail.com>
2015-06-10 09:25:25 +0200

    Reorganized ChecklistController.

--- src/Controller/ChecklistController.php
+++ src/Controller/ChecklistController.php
@@ -18,12 +18,13 @@ use Drupal\security_review\SecurityReview;
  */
 class ChecklistController {
   /**
+   * Creates the Run & Review page.
+   *
    * @return array
-   *   The 'Run & Review' page.
+   *   The 'Run & Review' page's render array.
    */
   public function index() {
     $run_form = array();
-    $stored_results = array();
 
     // If the user has the required permissions, show the RunForm.
     if (\Drupal::currentUser()->hasPermission('run security checks')) {
@@ -32,59 +33,13 @@ class ChecklistController {
         ->getForm('Drupal\security_review\Form\RunForm');
 
       // Close the Run form if there are results.
-      if(SecurityReview::getLastRun() > 0){
+      if (SecurityReview::getLastRun() > 0) {
         $run_form['run_form']['#open'] = FALSE;
       }
     }
 
-    if (SecurityReview::getLastRun() > 0) {
-      $checks = array();
-      foreach(Checklist::getChecks() as $check){
-        /** @var Check $check */
-
-        $checkInfo = array(
-          'result' => CheckResult::SKIPPED,
-          'message' => 'The check hasn\'t been run yet.'
-        );
-
-        $lastResult = $check->lastResult();
-
-        if($lastResult != null){
-          $checkInfo['result'] = $check->isSkipped() ? CheckResult::SKIPPED : $lastResult->result();
-          $checkInfo['message'] = $check->getMessage($lastResult->result());
-        }
-
-        $checkInfo['help_link'] = \Drupal::l('Details', Url::fromRoute('security_review.help',
-          array(
-            'namespace' => $check->getMachineNamespace(),
-            'title' => $check->getMachineTitle()
-          )
-        ));
-
-        $toggle_text = $check->isSkipped() ? 'Enable' : 'Skip';
-        $checkInfo['toggle_link'] = \Drupal::l($toggle_text, Url::fromRoute('security_review.toggle',
-          array(
-            'js' => 'nojs',
-            'check_id' => $check->getUniqueIdentifier()
-          ),
-          array('query' => array(
-            'token' => \Drupal::csrfToken()->get($check->getUniqueIdentifier())
-          ))
-        ));
-
-        $checks[] = $checkInfo;
-      }
-
-      $stored_results = array(
-        '#theme' => 'run_and_review',
-        '#date' => SecurityReview::getLastRun(),
-        '#checks' => $checks,
-        '#attached' => array(
-          'library' => array('security_review/run_and_review'),
-        )
-      );
-
-    } else {
+    // Print the results if any.
+    if (SecurityReview::getLastRun() <= 0) {
       // If they haven't configured the site, prompt them to do so.
       if (!SecurityReview::isConfigured()) {
         drupal_set_message(t('It appears this is your first time using the Security Review checklist. Before running the checklist please review the settings page at !link to set which roles are untrusted.',
@@ -93,6 +48,72 @@ class ChecklistController {
       }
     }
 
-    return array($run_form, $stored_results);
+    return array($run_form, $this->results());
+  }
+
+  /**
+   * Creates the results' table.
+   *
+   * @return array
+   *   The render array for the result table.
+   */
+  private function results(){
+    // If there are no results return.
+    if (SecurityReview::getLastRun() <= 0) {
+      return array();
+    }
+
+    $checks = array();
+    foreach (Checklist::getChecks() as $check) {
+      /** @var Check $check */
+
+      // Initialize with defaults.
+      $checkInfo = array(
+        'result' => CheckResult::SKIPPED,
+        'message' => 'The check hasn\'t been run yet.'
+      );
+
+      // Get last result.
+      $lastResult = $check->lastResult();
+      if ($lastResult != NULL) {
+        $checkInfo['result'] = $check->isSkipped() ? CheckResult::SKIPPED : $lastResult->result();
+        $checkInfo['message'] = $check->getMessage($lastResult->result());
+      }
+
+      // Determine help link.
+      $checkInfo['help_link'] = \Drupal::l('Details', Url::fromRoute('security_review.help',
+        array(
+          'namespace' => $check->getMachineNamespace(),
+          'title' => $check->getMachineTitle()
+        )
+      ));
+
+      // Add toggle button.
+      $toggle_text = $check->isSkipped() ? 'Enable' : 'Skip';
+      $checkInfo['toggle_link'] = \Drupal::l($toggle_text, Url::fromRoute('security_review.toggle',
+        array(
+          'js' => 'nojs',
+          'check_id' => $check->getUniqueIdentifier()
+        ),
+        array(
+          'query' => array(
+            'token' => \Drupal::csrfToken()
+              ->get($check->getUniqueIdentifier())
+          )
+        )
+      ));
+
+      // Add to array of completed checks.
+      $checks[] = $checkInfo;
+    }
+
+    return array(
+      '#theme' => 'run_and_review',
+      '#date' => SecurityReview::getLastRun(),
+      '#checks' => $checks,
+      '#attached' => array(
+        'library' => array('security_review/run_and_review'),
+      )
+    );
   }
 }
