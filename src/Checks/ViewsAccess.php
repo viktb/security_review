312d723ac6b0e946621d1256dd5e934bf4030676
banviktor <viktor.ban@gmail.com>
2015-07-06 23:11:55 +0200

    Ported views_access.

---
+++ src/Checks/ViewsAccess.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Viktor
+ * Date: 2015.07.06.
+ * Time: 21:55
+ */
+
+namespace Drupal\security_review\Checks;
+
+use Drupal;
+use Drupal\Core\Url;
+use Drupal\views\Entity\View;
+use Drupal\security_review\Check;
+use Drupal\security_review\CheckResult;
+
+class ViewsAccess extends Check {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getNamespace() {
+    return 'Security Review';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return 'Views access';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function run() {
+    // If views is not enabled return with INFO.
+    if (!Drupal::moduleHandler()->moduleExists('views')) {
+      return $this->createResult(CheckResult::INFO);
+    }
+
+    $result = CheckResult::SUCCESS;
+    $findings = array();
+
+    $views = entity_load_multiple('view');
+    /** @var View[] $views */
+
+    foreach ($views as $view) {
+      if ($view->status()) {
+        foreach ($view->get('display') as $display_name => $display) {
+          $access = &$display['display_options']['access'];
+          if (isset($access) && $access['type'] == 'none') {
+            $findings[$view->id()][] = $display_name;
+          }
+        }
+      }
+    }
+
+    if (!empty($findings)) {
+      $result = CheckResult::FAIL;
+    }
+
+    return $this->createResult($result, $findings);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function help() {
+    $paragraphs = array();
+    $paragraphs[] = "Views can check if the user is allowed access to the content. It is recommended that all Views implement some amount of access control, at a minimum checking for the permission 'access content'.";
+
+    return array(
+      '#theme' => 'check_help',
+      '#title' => 'Views access',
+      '#paragraphs' => $paragraphs
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate(CheckResult $result) {
+    $findings = $result->findings();
+    if (empty($findings)) {
+      return array();
+    }
+
+    $paragraphs = array();
+    $paragraphs[] = "The following View displays do not check access.";
+
+    $items = array();
+    foreach ($findings as $view_id => $displays) {
+      $view = entity_load('view', $view_id);
+      foreach ($displays as $display) {
+        $items[] = Drupal::l(
+          $view->label() . ': ' . $display,
+          Url::fromRoute(
+            'entity.view.edit_display_form',
+            array(
+              'view' => $view_id,
+              'display_id' => $display
+            )
+          )
+        );
+      }
+    }
+
+    return array(
+      '#theme' => 'check_evaluation',
+      '#paragraphs' => $paragraphs,
+      '#items' => $items
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluatePlain(CheckResult $result) {
+    $findings = $result->findings();
+    if (empty($findings)) {
+      return '';
+    }
+
+    $output = '';
+    foreach ($findings as $view_id => $displays) {
+      $output .= "\t" . $view_id . ": " . implode(', ', $displays) . "\n";
+    }
+
+    return $output;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMessage($resultConst) {
+    switch ($resultConst) {
+      case CheckResult::SUCCESS:
+        return 'Views are access controlled.';
+      case CheckResult::FAIL:
+        return 'There are Views that do not provide any access checks.';
+      case CheckResult::INFO:
+        return 'Module views is not enabled.';
+      default:
+        return 'Unexpected result.';
+    }
+  }
+
+}
